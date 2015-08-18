4d242ab664c49954ba346756493535cac9f77a5e
dcouwenb <Dimitri.Couwenberg@macquarie.com>
2015-08-18 16:33:01 +1000

    first version

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stuffApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the stuffApp
+ */
+angular.module('stuffApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name stuffApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the stuffApp
+ */
+angular.module('stuffApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
